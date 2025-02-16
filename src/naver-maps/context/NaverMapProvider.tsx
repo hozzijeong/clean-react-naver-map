a58421bc0f374cda2c2b2c3504640226ed8bcefb
hozzijeong <ghwls0905@naver.com>
2025-02-16 18:14:11 +0900

    naver-map 이벤트 추가

--- src/naver-maps/context/NaverMapProvider.tsx
+++ src/naver-maps/context/NaverMapProvider.tsx
@@ -1,30 +1,61 @@
 import {
 	createContext,
 	PropsWithChildren,
+	useCallback,
 	useContext,
 	useEffect,
+	useMemo,
 	useState,
 } from 'react';
 
 const NaverMapContext = createContext<naver.maps.Map | null>(null);
 
 interface Props extends PropsWithChildren {
-	mapId: string;
+	mapElement: string | HTMLElement;
 	options?: naver.maps.MapOptions;
+	eventHandlers?: NaverMapEventHandlers;
 }
 
-// NOTE: mapId와 동일한 id를 가진 태그를 Provider내부에 함께 위치시킬 것
-export const NaverMapProvider = ({ children, options = {}, mapId }: Props) => {
+// NOTE: mapElement 동일한 id를 가진 태그를 Provider내부에 함께 위치시킬 것
+export const NaverMapProvider = ({
+	children,
+	options = {},
+	eventHandlers = {},
+	mapElement,
+}: Props) => {
 	const [map, setMap] = useState<naver.maps.Map | null>(null);
 
+	const handleAddEventListeners = useNaverMapEventListener();
 	useEffect(() => {
-		if (map !== null) return;
+		if (map) return;
 
-		setMap(new naver.maps.Map(mapId, { ...options }));
-	}, [map, mapId, options]);
+		const naverMap = new naver.maps.Map(mapElement, { ...options });
+
+		setMap(naverMap);
+
+		const removeListeners = handleAddEventListeners(naverMap, eventHandlers);
+
+		return () => {
+			if (map) {
+				naverMap.destroy();
+				removeListeners();
+			}
+		};
+	}, [eventHandlers, handleAddEventListeners, map, mapElement, options]);
+
+	const memoizedMap = useMemo(() => map, [map]);
 
 	return (
-		<NaverMapContext.Provider value={map}>{children}</NaverMapContext.Provider>
+		<NaverMapContext.Provider value={memoizedMap}>
+			<div
+				id='map'
+				style={{
+					width: '100%',
+					height: '400px',
+				}}
+			/>
+			<>{memoizedMap && children}</>
+		</NaverMapContext.Provider>
 	);
 };
 
@@ -36,4 +67,65 @@ export const useNaverMap = () => {
 	}
 
 	return context;
+};
+
+interface NaverMapEventHandlers {
+	addLayer?: (layer: naver.maps.Layer) => void;
+	bounds_changed?: (bounds: naver.maps.Bounds) => void;
+	center_changed?: (center: naver.maps.Coord) => void;
+	centerPoint_changed?: (centerPoint: naver.maps.Point) => void;
+	click?: (pointerEvent: PointerEvent) => void;
+	dblclick?: (pointerEvent: PointerEvent) => void;
+	doubletap?: (pointerEvent: PointerEvent) => void;
+	drag?: (pointerEvent: PointerEvent) => void;
+	dragend?: (pointerEvent: PointerEvent) => void;
+	dragstart?: (pointerEvent: PointerEvent) => void;
+	idle?: () => void;
+	init?: () => void;
+	keydown?: (keyboardEvent: KeyboardEvent) => void;
+	keyup?: (keyboardEvent: KeyboardEvent) => void;
+	longtap?: (pointerEvent: PointerEvent) => void;
+	mapType_changed?: (mapType: naver.maps.MapType) => void;
+	mapTypeId_changed?: (mapTypeId: naver.maps.MapTypeId | string) => void;
+	mousedown?: (pointerEvent: PointerEvent) => void;
+	mousemove?: (pointerEvent: PointerEvent) => void;
+	mouseout?: (pointerEvent: PointerEvent) => void;
+	mouseover?: (pointerEvent: PointerEvent) => void;
+	mouseup?: (pointerEvent: PointerEvent) => void;
+	panning?: () => void;
+	pinch?: (pointerEvent: PointerEvent) => void;
+	pinchend?: (pointerEvent: PointerEvent) => void;
+	pinchstart?: (pointerEvent: PointerEvent) => void;
+	projection_changed?: (projection: naver.maps.Projection) => void;
+	removeLayer?: (layername: string) => void;
+	resize?: () => void;
+	rightclick?: (pointerEvent: PointerEvent) => void;
+	size_changed?: (size: naver.maps.Size) => void;
+	tap?: (pointerEvent: PointerEvent) => void;
+	tilesloaded?: () => void;
+	touchend?: (pointerEvent: PointerEvent) => void;
+	touchmove?: (pointerEvent: PointerEvent) => void;
+	touchstart?: (pointerEvent: PointerEvent) => void;
+	twofingertap?: (pointerEvent: PointerEvent) => void;
+	zoom_changed?: (zoom: number) => void;
+	zoomend?: () => void;
+	zoomstart?: () => void;
+}
+
+export const useNaverMapEventListener = () => {
+	const handleAddEventListeners = useCallback(
+		(naverMap: naver.maps.Map, params: NaverMapEventHandlers) => {
+			const listeners = Object.entries(params).map(
+				([eventName, listeners]) => naverMap.addListener(eventName, listeners),
+				[]
+			);
+
+			return () => {
+				naverMap.removeListener(listeners);
+			};
+		},
+		[]
+	);
+
+	return handleAddEventListeners;
 };
