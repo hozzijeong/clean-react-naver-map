b50b02b383171d96fc185d01ee23fb8be80b2805
hozzijeong <ghwls0905@naver.com>
2025-03-02 17:36:14 +0900

    마커 관련 코드 변경

--- src/naver-maps/components/overlay/marker/Marker.tsx
+++ src/naver-maps/components/overlay/marker/Marker.tsx
@@ -1,9 +1,17 @@
-import { isValidElement, useCallback, useLayoutEffect, useRef } from 'react';
-import { MarkerIcon, MarkerOptions } from './marker.types';
+import {
+	forwardRef,
+	isValidElement,
+	memo,
+	useCallback,
+	useImperativeHandle,
+	useLayoutEffect,
+	useRef,
+} from 'react';
+import { ImperativeMarker, MarkerIcon, MarkerOptions } from './marker.types';
 import { useNaverMap } from '../../../context/NaverMapProvider';
 import { renderToStaticMarkup } from 'react-dom/server';
 
-export const convertNaverMapMarkerIcon = (icon?: MarkerIcon) => {
+const convertNaverMapMarkerIcon = (icon?: MarkerIcon) => {
 	if (typeof icon === 'object' && icon !== null && 'content' in icon) {
 		const { content, ...rest } = icon;
 
@@ -24,10 +32,22 @@ export const convertNaverMapMarkerIcon = (icon?: MarkerIcon) => {
 	return icon;
 };
 
-const Marker = (props: MarkerOptions) => {
+const Marker = forwardRef<ImperativeMarker, MarkerOptions>((props, ref) => {
 	const map = useNaverMap();
 
 	const markerRef = useRef<naver.maps.Marker | null>(null);
+
+	useImperativeHandle(ref, () => ({
+		getMarker: () => ({ marker: markerRef.current, markerId: props.markerId }),
+		setMarker: (options: Omit<MarkerOptions, 'markerId'>) => {
+			if (!markerRef.current) return;
+			const { icon, ...rest } = options;
+
+			const markerIcon = convertNaverMapMarkerIcon(icon);
+
+			markerRef.current.setOptions({ map, icon: markerIcon, ...rest });
+		},
+	}));
 
 	const renderMarker = useCallback(() => {
 		const { icon, ...rest } = props;
@@ -53,6 +73,6 @@ const Marker = (props: MarkerOptions) => {
 	}, [renderMarker]);
 
 	return <></>;
-};
+});
 
-export default Marker;
+export default memo(Marker);
--- src/naver-maps/components/overlay/marker/marker.types.ts
+++ src/naver-maps/components/overlay/marker/marker.types.ts
@@ -36,10 +36,10 @@ type MarkerShape = {
 // MarkerOptions 타입 정의
 // NOTE: map은 필수지만 훅 내부에서 map을 추가할 예정
 export interface MarkerOptions {
+	markerId?: string | number;
 	animation?: naver.maps.Animation; // 마커 애니메이션
 	position: naver.maps.Coord | naver.maps.CoordLiteral; // 마커 위치 (좌표)
 	icon?: MarkerIcon; // 마커 아이콘
-	reactIcon?: ReactNode;
 	shape?: MarkerShape; // 마커 인터랙션 영역
 	title?: string; // 마커 툴팁 (마우스 오버 시 표시)
 	cursor?: string; // 마우스 오버 시 커서 모양 (기본: 'pointer')
@@ -47,4 +47,13 @@ export interface MarkerOptions {
 	draggable?: boolean; // 드래그 가능 여부 (기본: false)
 	visible?: boolean; // 마커 표시 여부 (기본: true)
 	zIndex?: number; // 마커의 z-index 값 (레이어 순서)
+}
+
+// NOTE: 제대로된 getMarker를 얻기 위해선 1회 렌더링된 이후에 호출해야 얻을 수 있다 (marker자체가 layoutEffect에서 생성되기 때문)
+export interface ImperativeMarker {
+	getMarker: () => {
+		marker: naver.maps.Marker | null;
+		markerId?: string | number;
+	};
+	setMarker: (options: MarkerOptions) => void;
 }
